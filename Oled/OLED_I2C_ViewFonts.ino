b3091fb546549a9f7ce6e1c977594cc8805d5410
wangga03 <aswanggasadu@gmail.com>
2024-06-03 14:00:27 +0700

    menambahkan codingan untuk menampilkan pada oled berupa tipe data int/float

---
+++ Oled/OLED_I2C_ViewFonts.ino
@@ -0,0 +1,28 @@
+
+
+#include <OLED_I2C.h>
+
+OLED  myOLED(SDA, SCL); // Remember to add the RESET pin if your display requires it...
+
+extern uint8_t SmallFont[];
+extern uint8_t TinyFont[];
+extern uint8_t BigNumbers[];
+
+void setup()
+{
+  if(!myOLED.begin(SSD1306_128X32))
+    while(1);   // In case the library failed to allocate enough RAM for the display buffer...
+}
+
+void loop()
+{
+  float i = 10.0;
+  myOLED.setFont(BigNumbers);
+  myOLED.clrScr();
+  myOLED.printNumF(10, 0, CENTER, 1);
+  myOLED.update();
+  delay (5000);
+
+}
+
+
