c9f93ad9bd99d0ee21c9f446e23dc53878414532
wangga03 <aswanggasadu@gmail.com>
2024-06-11 21:01:09 +0700

    lasihd

--- conveyor.py
+++ conveyor.py
@@ -7,7 +7,7 @@ x_start = 100
 y_start = 250
 x_end = 500
 y_end = 480
-# arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=.1) 
+arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=.1) 
 def main(capture) :
 
     width = 640
@@ -81,7 +81,7 @@ def main(capture) :
             if lastCounter == counter :
                 counter+=1
             counterToArduino = str(f"{lastCounter} ")
-            # arduino.write(counterToArduino.encode())
+            arduino.write(counterToArduino.encode())
             # cv.putText(frame, 'Bata Hitam', (x, y+50), font, 1, colour_green, 2)
             # cv.rectangle(frame, (x, y), (w + x, h + y), colour_green, 2)
 
