4a44173215893d76c47936a7f87a5ee05c927277
wangga03 <aswanggasadu@gmail.com>
2024-06-12 23:42:51 +0700

    lksdhjf

--- conveyor.py
+++ conveyor.py
@@ -81,7 +81,7 @@ def main(capture) :
             if lastCounter == counter :
                 counter+=1
             # cv.putText(frame, 'Bata Hitam', (x, y+50), font, 1, colour_green, 2)
-            cv.rectangle(frame, (x, y), (w + x, h + y), colour_green, 2)
+            # cv.rectangle(frame, (x, y), (w + x, h + y), colour_green, 2)
 
         else :
             if status == True :
@@ -93,11 +93,11 @@ def main(capture) :
         arduino.write(counterToArduino.encode())
         print(f"ret : {ret}")
         print(f"hasil hitung : {lastCounter}")
-        cv.imshow("frame", frame)
-        cv.imshow("frame2", frame2)
-        # cv.imshow("frame crop", frameCrop)
-        cv.imshow("tresh green", tresh_green)
-        cv.imshow("tresh white", tresh_white)
+        # cv.imshow("frame", frame)
+        # cv.imshow("frame2", frame2)
+        # # cv.imshow("frame crop", frameCrop)
+        # cv.imshow("tresh green", tresh_green)
+        # cv.imshow("tresh white", tresh_white)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
     
